484feaacd66a9f449570027ddfef232c67df2063
amartya-dev <amarkaushik1999@gmail.com>
2020-03-16 00:26:26 +0530

    signup screen

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -41,3 +41,57 @@ class Button extends StatelessWidget {
     );
   }
 }
+
+class ButtonWithIcon extends StatelessWidget {
+  final buttonColor;
+  final buttonText;
+  final textColor;
+  final borderColor;
+  final icon;
+  final Function buttonAction;
+
+  ButtonWithIcon(
+    this.buttonColor,
+    this.buttonText,
+    this.textColor,
+    this.buttonAction,
+    this.borderColor,
+    this.icon,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.85,
+      height: MediaQuery.of(context).size.width * 0.16,
+      child: RaisedButton(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            icon,
+            Container(
+              margin: EdgeInsets.all(6),
+              child: Text(
+                buttonText,
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+        onPressed: buttonAction,
+        color: buttonColor,
+        textColor: textColor,
+        shape: StadiumBorder(
+          side: BorderSide(
+            color: borderColor,
+            width: 2,
+          ),
+        ),
+      ),
+      margin: EdgeInsets.all(10),
+    );
+  }
+}
--- lib/signup.dart
+++ lib/signup.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 import './buttons.dart';
+import 'buttons.dart';
+import './icons/my_flutter_app_icons.dart';
+
 
 class SignUp extends StatelessWidget {
   void signUp() => print("Signup");
@@ -9,6 +14,7 @@ class SignUp extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
+        Image.asset('assets/images/logo_orange.png'),
         Button(
           Color.fromRGBO(254, 70, 0, 0.8),
           "LOGIN",
@@ -23,19 +29,21 @@ class SignUp extends StatelessWidget {
           signUp,
           Color.fromRGBO(254, 70, 0, 0.8),
         ),
-        Button(
+        ButtonWithIcon(
           Colors.white,
           "SIGN IN WITH APPLE",
           Colors.black,
           signUp,
           Colors.black,
+          Icon(MyFlutterApp.apple_logo_black),
         ),
-        Button(
+        ButtonWithIcon(
           Color.fromRGBO(59, 89, 153, 0.9),
           "SIGN IN WITH FACEBOOK",
           Colors.white,
           signUp,
           Color.fromRGBO(59, 89, 153, 0.9),
+          Icon(FontAwesomeIcons.facebook)
         )
       ],
     ));
